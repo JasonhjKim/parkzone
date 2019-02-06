e4aae5d7e247ea7472dc0e6f7c3502c5f341a1df
Hyun Jun (Jason) Kim <jasonhjkim1205@gmail.com>
2019-02-06 01:10:53 -0500

    Register (#1)
    * made changes to register screen
    
    * yooo more build files
    
    * deleted ios/build files

--- src/screens/register.js
+++ src/screens/register.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TextInput } from 'react-native';
+import styled from 'styled-components';
 
 export default class Register extends Component {
     render() {
         return(
             <View>
-                <Text>This is the register screen</Text>
+                <Text>Welcome to Register Screen niggas</Text>
+                <TextInput placeholder="Full Name" textContentType="name"/>
+                <TextInput placeholder="Email" textContentType="emailAddress"/>
+                <TextInput placeholder="Password" textContentType="password"/>
+                <TextInput placeholder="Confirm Password" textContentType="password"/>
                 <Button title="To Landing" onPress={ () => this.props.navigation.navigate("Landing")}></Button>
             </View>
         )
     }
 }
+
+const FullView = styled.View`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+`
