2f5f502865fc0cf322cf3f780203e1f5a8939937
Jasonhjkim <jasonhjkim1205@gmail.com>
2019-02-11 22:37:45 -0500

    added styled examples

--- src/commons/fontSize.js
+++ src/commons/fontSize.js
@@ -10,6 +10,7 @@
 
 import { Dimensions, Platform, PixelRatio, Text } from 'react-native';
 import React from 'react';
+import { StyleSheet } from 'react-native';
 
 function calculateSize(size) {
     const { width: device_width, height: device_height} =  Dimensions.get('window');
@@ -28,36 +29,36 @@ function calculateSize(size) {
 
 //H1 
 export const H1 = (props) => (
-    <Text {...props} style={{fontSize: calculateSize(40), fontWeight: "bold"}}></Text>
+    <Text {...props} style={{fontSize: calculateSize(40), fontWeight: "bold", color: props.color ? props.color  : "black" }}></Text>
 )
 
 //Recommend using h2 or h3 for headings
 export const H2 = (props) => (
-    <Text {...props} style={{fontSize: calculateSize(28), fontWeight: "bold"}}></Text>
+    <Text {...props} style={{fontSize: calculateSize(28), fontWeight: "bold", color: props.color ? props.color  : "black" }}></Text>
 )
 
 export const H3 = (props) => (
-    <Text {...props} style={{fontSize: calculateSize(22), fontWeight: "bold"}}></Text>
+    <Text {...props} style={{fontSize: calculateSize(22), fontWeight: "bold", color: props.color ? props.color  : "black" }}></Text>
 )
 
 //Recommend using h4 for subheadings
 export const H4 = (props) => (
-    <Text {...props} style={{fontSize: calculateSize(20), fontWeight: "bold"}}></Text>    
+    <Text {...props} style={{fontSize: calculateSize(20), fontWeight: "bold", color: props.color ? props.color  : "black" }}></Text>    
 )
 
 export const H5 = (props) => (
-    <Text {...props} style={{fontSize: calculateSize(18), fontWeight: "bold"}}></Text>    
+    <Text {...props} style={{fontSize: calculateSize(18), fontWeight: "bold", color: props.color ? props.color  : "black" }}></Text>    
 )
 
 export const H6 = (props) => (
-    <Text {...props} style={{fontSize: calculateSize(16), fontWeight: "bold"}}></Text>    
+    <Text {...props} style={{fontSize: calculateSize(16), fontWeight: "bold", color: props.color ? props.color  : "black" }}></Text>    
 )
 
 //Regular Text => You can use this for most of your basic texts
 export const T = (props) => (
-    <Text {...props} style={{fontSize: calculateSize(16)}}></Text>   
+    <Text {...props} style={{fontSize: calculateSize(16), color: props.color ? props.color : "black" }}></Text>   
 )
 
 export const P = (props) => (
-    <Text {...props} style={{fontSize: calculateSize(12)}}></Text>
+    <Text {...props} style={{fontSize: calculateSize(12), color: props.color ? props.color : "black" }}></Text>
 )
--- src/components/exampleFont.js
+++ src/components/exampleFont.js
@@ -1,6 +1,7 @@
 import { H1, H2, H3, H4, H5, H6, T, P} from '../commons/fontSize';
 import React, { Component} from 'react';
 import { View } from 'react-native';
+import styled from 'styled-components/native';
 
 export default class ExampleFont extends Component {
     render() {
@@ -14,7 +15,21 @@ export default class ExampleFont extends Component {
                 <H6>Example H6 Text</H6>
                 <T>Example T Text</T>
                 <P>Example P Text</P>
+
+                { /* apparently cascading styling for react-native is still in development, so use props to pass colour down 
+                   * but you can still use styled(H1) to change font-size using cascading style
+                   */ }
+                <StyledH1 color="red">Example Styled H1 Text</StyledH1>
+                <StyledT color="salmon">Example Styled T Text</StyledT>
             </View>
         )
     }
 }
+
+const StyledH1 = styled(H1)`
+    font-size: 60px;
+`
+
+const StyledT = styled(T)`
+    color: lightgray;
+`
