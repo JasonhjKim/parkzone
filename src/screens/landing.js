1caf25f7a10364befac760ac8fd581160b2885c7
Jason Shi <46632519+litesoul00@users.noreply.github.com>
2019-02-13 20:32:59 -0800

    Landing (#4)
    * added landing
    
    * added landing
    
    * fixed spacing
    
    * added roboto

--- src/screens/landing.js
+++ src/screens/landing.js
@@ -1,15 +1,59 @@
 import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image, TouchableHighlight } from 'react-native';
+import Logo from '../assets/PPP.png';
+import styled from 'styled-components/native';
+import FullView from '../components/fullView';
+import StyledButton from '../components/button';
 import { View, Text, Button } from 'react-native';
 import { H1 } from '../commons/fontSize';
 
-export default class Landing extends Component {
+export default class Landing extends React.Component {
     render() {
-        return(
-            <View>
-                <Text>This is the landing screen</Text>
-                <H1>Some shit</H1>
-                <Button title="To Login" onPress={ () => this.props.navigation.navigate("Login")}></Button>
-            </View>
-        )
+        console.log ( this.props )
+        return (
+            <StyledFullView>
+                {/* <LogoContainer> */}
+                        {/* <Image source={Logo} style={StyleSheet.image}/> */}
+                {/* </LogoContainer> */}
+                <ChildContainer>
+                    <StyledButton
+                        title = "Login"
+                        onPress ={() => this.props.navigation.navigate( "Login" )}
+                        theme = 'secondary'
+                    />
+                    <StyledButton 
+                        title = "Register"
+                        onPress ={() => this.props.navigation.navigate( "Register" )}
+                    />
+                </ChildContainer>
+            </StyledFullView>
+        );
     }
 }
+
+const StyledFullView = styled( FullView )`
+    justify-content: flex-start;
+    align-items: center;
+    flex-direction: column;
+`
+const ChildContainer = styled.View`
+    width: auto;
+    height:115px;
+    flex-direction: column;
+    justify-content: space-between;
+    margin: 325px;
+`
+// const LogoContainer = styled.View`
+    // width:100px;
+    // height: 100px;
+    // margin: 15px 0;
+    // flex-direction: column;
+// `
+
+
+
+// const Logo = require ('../assets/PPP.png');
+// const LogoImage = styled.Image`
+    // width: 50%;
+    // height: 50%;
+// `
